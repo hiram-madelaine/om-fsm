a76e71bbdd8a822729effba9c01e6d2fb8b025cb
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-06-16 10:30:43 +0200

    Simplification : Handle fsm state in on place: value

--- src/om_automat/core.cljs
+++ src/om_automat/core.cljs
@@ -9,13 +9,11 @@
 (enable-console-print!)
 
 
-;; define your app data so that it doesn't get over-written on reload
-
 (defonce app-state (atom { }))
 
 (defn display?
   [s k]
-  (get-in s [:value k :display]))
+  (get-in s [:value :view  k :display]))
 
 (defn fsm-component
   [app owner]
@@ -23,8 +21,12 @@
     om/IInitState
     (init-state [_]
       (let [inputs (interpose (a/$ :next) [:start :one :two (a/* :three) :end])
-            fsm (a/compile inputs {:reducers {:next (fn [view-state action]
-                                                      (assoc-in view-state [action :display] true))}})]
+            fsm (a/compile inputs {:reducers {:next  (fn [view-state input]
+                                                       (-> view-state
+                                                           (assoc-in [:view input :display] true)
+                                                           (assoc :last-input input)
+                                                           (update-in  [:counters input] inc)))
+                                              }})]
         {:inputs inputs
          :adv        (partial a/advance fsm)
          :chan/fsm   (chan)
@@ -34,23 +36,18 @@
       (let [{:keys [adv chan/fsm init-state]} (om/get-state owner)]
         (go-loop []
                  (let [r (adv init-state :start)]
-                   (om/set-state! owner :fsm-state r))
+                   (om/set-state! owner :fsm-state r))      ;First transition is automatic.
                  (loop []
-                   (let [a (<! fsm)
-                         fsm-state (om/get-state owner :fsm-state)
-                         next-state (adv fsm-state a :ko)]
-                     (om/set-state! owner :last-input a)
-                     (if (= :ko next-state)
-                       (om/set-state! owner :rejected a)
-                       (do
-                         (om/set-state! owner :rejected nil)
-                         (om/set-state! owner :fsm-state next-state)))
-                     (when-not (:accepted? next-state)
+                   (let [input (<! fsm)
+                         fsm-state (om/get-state owner :fsm-state) ;Get the current fsm state from local state.
+                         next-state (adv fsm-state input :ko)] ;Try to advance the fsm with an input.
+                     (when-not (= :ko next-state)
+                       (om/set-state! owner :fsm-state next-state)) ;The next state is saved in local state
+                     (when-not (:accepted? next-state)      ;We continue until the fsm is in an accepted state.
                        (recur))))
-                 (recur))
-        ))
+                 (recur))))
     om/IRenderState
-    (render-state [_ {:keys [chan/fsm fsm-state rejected last-input] :as state}]
+    (render-state [_ {:keys [chan/fsm fsm-state last-input] :as state}]
       (dom/div #js {:className "container"}
                (dom/div #js {:className "well"}
                         (dom/p #js {}
@@ -58,16 +55,16 @@
                                The view is simple : there is a button for each transition"))
                (dom/div #js {:className "well"}
                 (b/toolbar {}
-                           (when (get-in fsm-state [:value :start :display])
+                           (when (display? fsm-state :start)
                              (b/button {:onClick  #(put! fsm :one)
                                         :bs-style "danger"} "One"))
-                           (when (get-in fsm-state [:value :one])
+                           (when (display? fsm-state :one)
                              (b/button {:onClick  #(put! fsm :two)
                                         :bs-style "primary"} "Two"))
-                           (when (get-in fsm-state [:value :two])
+                           (when (display? fsm-state :two)
                              (b/button {:onClick  #(put! fsm :three)
                                         :bs-style "success"} "Three *"))
-                           (when (get-in fsm-state [:value :three])
+                           (when (display? fsm-state :three)
                              (b/button {:onClick  #(put! fsm :end)
                                         :bs-style "warning"} "End")))
                         (when last-input (dom/div #js {:className "well"}
@@ -75,10 +72,10 @@
                (let [{:keys [state-index value stream-index]} fsm-state]
                  (dom/div #js {:className "well"}
                           (dom/label #js {} "FSM")
-                          (dom/p #js {} (str "value : " value))
+                          (dom/p #js {} (str "View state : " (:view value)))
+                          (dom/p #js {} (str "Counters : " (:counters value)))
                           (dom/p #js {} (str "state-index : " state-index))
-                          (dom/p #js {} (str "stream-index : " stream-index))
-                          (when rejected (dom/p #js {} "Transition rejected : " (name rejected)))))))))
+                          (dom/p #js {} (str "stream-index : " stream-index))))))))
 
 
 (om/root
